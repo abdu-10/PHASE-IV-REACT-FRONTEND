b617fbf0bb00736c2388663ea1c04f66f1935ddc
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-03-04 13:38:34 +0300

    improves UX and dashboard for owners

--- src/components/owners/OwnerDashboard.jsx
+++ src/components/owners/OwnerDashboard.jsx
@@ -78,42 +78,80 @@ function OwnerDashboard() {
         </div>
       </nav>
 
-      
-
-     
-<div class="lg:px-20 md:px-6 px-4 md:py-12 py-8">
-  
-  <div class="lg:flex items-center justify-between">
-    <div class="lg:w-1/3">
-      <h1 class="text-4xl font-semibold leading-9 text-gray-800 dark:text-black">Turn your motorcycles into a business</h1>
-      <p class="text-base leading-6 mt-4 text-l-600 dark:text-black">
-List your motorcycle for free and make an average of $100 per booking. 
-We strive to turn only the safest riders into renters by using technology.
-
-.</p>
-      <button role="button" aria-label="view catalogue" class="focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none mt-6 md:mt-8 text-base font-semibold leading-none text-gray-800 dark:text-black flex items-center hover:underline">
-        <NavLink to="/owner/new-bike">List Your Motorcycle Today</NavLink>
-        <svg class="ml-2 mt-1 dark:text-white" width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1.33325 4H10.6666" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M8 6.66667L10.6667 4" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-          <path d="M8 1.33398L10.6667 4.00065" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-      </button>
-    </div>
-    <div class="lg:w-7/12 lg:mt-10 mt-8">
-      <div class="w-full h-full bg-red-200">
-        <img src="https://rare-gallery.com/uploads/posts/373251-4k-wallpaper.jpg" alt="apartment design" class="w-full sm:block hidden" />
-        <img src="https://rare-gallery.com/uploads/posts/373251-4k-wallpaper.jpg" alt="apartment design" class="sm:hidden block w-full" />
+      <div class="lg:px-20 md:px-6 px-4 md:py-12 py-8">
+        <div class="lg:flex items-center justify-between">
+          <div class="lg:w-1/3">
+            <h1 class="text-4xl font-semibold leading-9 text-gray-800 dark:text-black">
+              Turn your motorcycles into a business
+            </h1>
+            <p class="text-base leading-6 mt-4 text-l-600 dark:text-black">
+              List your motorcycle for free and make an average of $100 per
+              booking. We strive to turn only the safest riders into renters by
+              using technology. .
+            </p>
+            <button
+              role="button"
+              aria-label="view catalogue"
+              class="focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none mt-6 md:mt-8 text-base font-semibold leading-none text-gray-800 dark:text-black flex items-center hover:underline"
+            >
+              <NavLink to="/owner/new-bike">List Your Motorcycle Today</NavLink>
+              <svg
+                class="ml-2 mt-1 dark:text-white"
+                width="12"
+                height="8"
+                viewBox="0 0 12 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.33325 4H10.6666"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8 6.66667L10.6667 4"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8 1.33398L10.6667 4.00065"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+          <div class="lg:w-7/12 lg:mt-10 mt-8">
+            <div class="w-full h-full bg-red-200">
+              <img
+                src="https://rare-gallery.com/uploads/posts/373251-4k-wallpaper.jpg"
+                alt="apartment design"
+                class="w-full sm:block hidden"
+              />
+              <img
+                src="https://rare-gallery.com/uploads/posts/373251-4k-wallpaper.jpg"
+                alt="apartment design"
+                class="sm:hidden block w-full"
+              />
+            </div>
+            <div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 lg:gap-8 gap-6 lg:mt-8 md:mt-6 mt-4">
+              <img
+                src="https://badasshelmetstore.com/wp-content/uploads/2020/08/1707849-scaled.jpg"
+                class="w-full"
+                alt="Red Bike"
+              />
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGs8I6iOFqRi7bEGm6QqcJZeREVV0SExZng&usqp=CAU"
+                class="w-full"
+                alt="Bike"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 lg:gap-8 gap-6 lg:mt-8 md:mt-6 mt-4">
-        <img src="https://badasshelmetstore.com/wp-content/uploads/2020/08/1707849-scaled.jpg" class="w-full" alt="Red Bike" />
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGs8I6iOFqRi7bEGm6QqcJZeREVV0SExZng&usqp=CAU" class="w-full" alt="Bike" />
-      </div>
-    </div>
-  </div>
-</div>
-
-
     </>
   );
 }
